43ab9c889d66225feffbf43688dcd958fc65fd62
Shekhov Ismoil <veyxov@gmail.com>
2022-09-16 22:10:48 +0500

    base connection and echo.

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+class Program {
+    static TelegramBotClient bot = new TelegramBotClient(Environment.GetEnvironmentVariable("TOKEN")!);
+    static void Main() {
+        var receiverOptions = new ReceiverOptions()
+        {
+            AllowedUpdates = new UpdateType[]
+            {
+                UpdateType.Message,
+                UpdateType.EditedMessage
+            }
+        };
+
+        bot.StartReceiving(UpdateHandler, ErrorHandler, receiverOptions);
+
+        Console.ReadKey();
+    }
+
+    static Task ErrorHandler(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+    {
+        throw new NotImplementedException();
+    }
+
+    static async Task UpdateHandler(ITelegramBotClient arg1, Update update, CancellationToken arg3)
+    {
+        if (update.Type == UpdateType.Message)
+        {
+            var text = update?.Message?.Text;
+            var id = update?.Message?.Chat.Id;
+            var username = update?.Message?.Chat.Username;
+
+            Message sent = await bot.SendTextMessageAsync(id!, "You said: " + text, cancellationToken: CancellationToken.None);
+
+            Console.WriteLine($"{text} {id} {username}");
+        }
+    }
+}
