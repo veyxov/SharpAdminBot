ff590933f6d84ede872784e111b1393c2e8dc358
Shekhov Ismoil <veyxov@gmail.com>
2022-09-17 16:04:04 +0500

    moved the error handling logic to separate file

--- Bot/GlobalUpdateHandler.cs
+++ Bot/GlobalUpdateHandler.cs
@@ -6,6 +6,13 @@ public static class GlobalLogic
     public static async Task UpdateHandler(ITelegramBotClient arg1, Update update, CancellationToken arg3)
     {
         Log.Warning("{@arst}", JsonSerializer.Serialize(update, new JsonSerializerOptions() {DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull, WriteIndented = true}));
+    }
 
+    public static Task ErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken cancellationToken)
+    {
+        // TODO: Handle errors
+        Log.Fatal("An exception occurred {@exception}", exception);
+
+        throw new Exception(exception.Message);
     }
 }
--- Bot/Program.cs
+++ Bot/Program.cs
@@ -4,17 +4,8 @@
     static void Main() {
         ConfigurationExtensions.ConfigureLogging();
 
-        bot.StartReceiving(GlobalLogic.UpdateHandler, ErrorHandler, new ReceiverOptions() { AllowedUpdates = Array.Empty<UpdateType>() });
+        bot.StartReceiving(GlobalLogic.UpdateHandler, GlobalLogic.ErrorHandler, new ReceiverOptions() { AllowedUpdates = Array.Empty<UpdateType>() });
 
         Console.ReadKey();
     }
-
-    static Task ErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken cancellationToken)
-    {
-        // TODO: Handle errors
-        Log.Fatal("An exception occurred {@exception}", exception);
-
-        throw new Exception(exception.Message);
-    }
-
 }
